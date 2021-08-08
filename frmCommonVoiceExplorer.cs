7dbd98ae5d53bf98193961e82919738b827838ee
Ardin <Ardin@gmail.com>
2021-08-08 16:10:55 +0430

    first commit

---
+++ frmCommonVoiceExplorer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DubleKon.ir
+{
+    public partial class frmCommonVoiceExplorer : Form
+    {
+        List<Speech> list = new List<Speech>();
+        public string RootPath { get; set; }
+        public frmCommonVoiceExplorer()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                btnCheckAllFiles.Invoke((MethodInvoker)delegate
+                {
+                    FolderBrowserDialog folder = new FolderBrowserDialog();
+                    if (folder.ShowDialog() == DialogResult.OK)
+                    {
+                        RootPath = folder.SelectedPath;
+                        var lines = File.ReadAllLines(RootPath + "\\validated.tsv");
+
+                        foreach (var line in lines)
+                        {
+                            var detail = line.Split(new string[] { "\t" }, StringSplitOptions.None);
+                            Speech speech = new Speech()
+                            {
+                                ClientId = detail[0],
+                                Path = detail[1],
+                                Sentence = detail[2],
+                                Age = detail[5],
+                                Gender = detail[6],
+                            };
+                            list.Add(speech);
+                        }
+
+                        var groupedByOrderedList = list.GroupBy(a => a.ClientId).OrderBy(a => a.Count());
+
+                        foreach (var item in groupedByOrderedList)
+                        {
+                            listBox1.Items.Insert(0, item.Count() + " - " + item.First().ClientId);
+                        }
+                    }
+                });
+            });
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                btnCheckAllFiles.Invoke((MethodInvoker)delegate
+                {
+                    var selected = listBox1.Items[listBox1.SelectedIndex].ToString().Split('-').Last().Trim();
+                    var items = list.Where(a => a.ClientId == selected).OrderBy(a => Guid.NewGuid());
+
+                    txtClientId.Text = items.First().ClientId;
+                    lblAge.Text = items.First().Age;
+                    lblGender.Text = items.First().Gender;
+
+                    listBox2.Items.Clear();
+                    foreach (var item in items)
+                    {
+                        listBox2.Items.Add(item.Sentence + " - " + item.Path);
+                    }
+                });
+            });
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                btnCheckAllFiles.Invoke((MethodInvoker)delegate
+                {
+                    var speek = listBox2.Items[listBox2.SelectedIndex].ToString().Split('-').Last().Trim();
+                    string path = RootPath + "\\clips\\" + speek;
+                    WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer
+                    {
+                        URL = path
+                    };
+                    wplayer.controls.play();
+                });
+            });
+        }
+        private void btnLoadInfo_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                btnCheckAllFiles.Invoke((MethodInvoker)delegate
+                {
+                    var items = list.Where(a => a.ClientId == txtClientId.Text).OrderBy(a => Guid.NewGuid());
+
+                    txtClientId.Text = items.First().ClientId;
+                    lblAge.Text = items.First().Age;
+                    lblGender.Text = items.First().Gender;
+
+                    listBox2.Items.Clear();
+                    foreach (var item in items)
+                    {
+                        listBox2.Items.Add(item.Sentence + " - " + item.Path);
+                    }
+                });
+            });
+        }
+        private void btnCheckAllFiles_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                btnCheckAllFiles.Invoke((MethodInvoker)delegate
+                {
+                    var items = list.Where(a => a.ClientId == txtClientId.Text).ToList();
+                    int exist = 0;
+                    foreach (var item in items)
+                    {
+                        string path = RootPath + "\\clips\\" + item.Path;
+                        if (File.Exists(path))
+                            exist++;
+                    }
+                    MessageBox.Show("Exist: " + exist + "\r\nNot exist: " + (items.Count - exist));
+                });
+            });
+        }
+    }
+    public class Speech
+    {
+        public string ClientId { get; set; }
+        public string Path { get; set; }
+        public string Sentence { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ frmCommonVoiceExplorer.designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace DubleKon.ir
+{
+    partial class frmCommonVoiceExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.txtClientId = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.btnLoadInfo = new System.Windows.Forms.Button();
+            this.btnCheckAllFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(93, 394);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Client Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gender:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(114, 121);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(674, 316);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Location = new System.Drawing.Point(165, 38);
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(542, 20);
+            this.txtClientId.TabIndex = 4;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(162, 65);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(13, 13);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "0";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(162, 92);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(10, 13);
+            this.lblGender.TabIndex = 2;
+            this.lblGender.Text = "-";
+            // 
+            // btnLoadInfo
+            // 
+            this.btnLoadInfo.Location = new System.Drawing.Point(713, 36);
+            this.btnLoadInfo.Name = "btnLoadInfo";
+            this.btnLoadInfo.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadInfo.TabIndex = 5;
+            this.btnLoadInfo.Text = "load";
+            this.btnLoadInfo.UseVisualStyleBackColor = true;
+            this.btnLoadInfo.Click += new System.EventHandler(this.btnLoadInfo_Click);
+            // 
+            // btnCheckAllFiles
+            // 
+            this.btnCheckAllFiles.Location = new System.Drawing.Point(697, 65);
+            this.btnCheckAllFiles.Name = "btnCheckAllFiles";
+            this.btnCheckAllFiles.Size = new System.Drawing.Size(91, 23);
+            this.btnCheckAllFiles.TabIndex = 5;
+            this.btnCheckAllFiles.Text = "Check all files";
+            this.btnCheckAllFiles.UseVisualStyleBackColor = true;
+            this.btnCheckAllFiles.Click += new System.EventHandler(this.btnCheckAllFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCheckAllFiles);
+            this.Controls.Add(this.btnLoadInfo);
+            this.Controls.Add(this.txtClientId);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Common Voice Explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox txtClientId;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Button btnLoadInfo;
+        private System.Windows.Forms.Button btnCheckAllFiles;
+    }
+}
+
